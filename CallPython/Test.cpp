86eb6ed088df206c86b2624f64dd432e41287a0d
pengjianaixue <pengjianaixue@hotmail.com>
2019-02-27 17:34:09 +0800

    error info avavilable and python file path add available

--- CallPython/Test.cpp
+++ CallPython/Test.cpp
@@ -2,22 +2,27 @@
 #include "CallPython.h"
 void run()
 {
-
+	
 	CCallPython a;
+	a.SetPyPath(R"(C:/Users/eijpnae/Desktop)");//C++ Raw string synatx  the path add to python need the slash not the backslash
 	try
 	{	
 		
 		PARAMLIST paramlist;
-		PARAMPAIR param;
-		param.first = "s";
+		PARAMPAIR param,param1;
+		param.first = "i"; 
 		param.second = "117";
+		param1.first = "s";
+		param1.second = "18800000";
 		paramlist.push_back(param);
+		paramlist.push_back(param1);
 		std::string res = a.Runfunction("main", "main", paramlist);
 		printf(res.c_str());
 
 	}
 	catch (const std::exception& e)
 	{
+
 		printf(e.what());
 
 	}

dd84bb19e06717da3bd89a4bcb8e43b08ac31b31
jian peng <jian.peng@ericsson.com>
2020-01-10 17:09:34 +0800

    finish the runPyFunction code

--- PyCaller/Test.cpp
+++ PyCaller/Test.cpp
@@ -7,16 +7,6 @@ void run()
 	PyCaller a(R"(C:/Users/eijpnae/Desktop)");
 	try
 	{			
-		//a.runPyFile(R"(.\test.py)");
-		PyCaller::PYPARAMLIST paramlist;
-		PyCaller::PYPARAMPAIR param, param1;
-		PyCaller::PyResult res = nullptr;
-		param.first = "d";
-		param.second = "117.58";
-		param1.first = "d";
-		param1.second = "118.56";
-		paramlist.push_back(param);
-		paramlist.push_back(param1);
 	 	double sumValue{0};
 		bool executeRes = a.runPyFunction(R"(main)", "testFetch", 15.45,118.56,153.0,4,5,6,7, sumValue);
 		if (executeRes)
